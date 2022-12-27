f92dcc7451d1483e614e716b86ba4d53f9418cfd
JMLatino <38223339+JMLatino@users.noreply.github.com>
2022-12-27 22:22:17 +0000

    added example

--- my-element.js
+++ my-element.js
@@ -45,18 +45,50 @@ export class MyElement extends LitElement {
     super();
     this.name = 'World';
     this.count = 0;
+    this.players = [
+      {name: 'Ronaldo', img: 'https://cdn.nos.nl/image/2022/11/24/919035/768x576a.jpg'},
+      {name: 'Messi', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/640px-Lionel_Messi_WC2022.jpg'},
+      {name: 'Mbappé', img: 'https://ronaldo.com/wp-content/uploads/2019/09/GettyImages-1159265415.jpg'},
+      {name: 'Neymar', img: 'https://media.nu.nl/m/2m2x54gamlhw_wd854/neymar.jpg'},
+      {name: 'de Jong', img: 'https://vp.cdn.pxr.nl/news/2022/12/20/v2_large_97d9760e1867827bd773a20a4af2cff37de5314c.jpg'},
+      {name: 'Hakimi', img: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltf4389e5af01ea4c3/60dab80f892a730f58805a04/ae5d85fe4883d988d81024996c9b8892d664c4e4.jpg'},
+      {name: 'Modrić', img: 'https://img.a.transfermarkt.technology/portrait/header/27992-1661426133.jpg?lm=1'},
+      
+    ];
+    this.selectedPlayer = '';
   }
 
   render() {
     return html`
-      <h1>${this.sayHello(this.name)}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
-    `;
+      ${this.players
+        .map(
+          player => this.selectedPlayer.length == 0 || this.selectedPlayer === player.name 
+          ?  html`
+          <button @click="${() => this.setPlayer(player.name)}">
+            <h5 class="card-title">${player.name}</h5>
+            <img width="200" src="${player.img}" class="card-img-top" >
+          </button>
+          `
+          :``)
+        }`
+
+    // return html`
+    //   <h1>Request Insurance</h1>
+    //   <label>Name</label> </br>
+    //   <input type="text" /> </br>
+    //   <label>Date of birth</label> </br>
+    //   <input type="date" /> </br>
+    //   <label>Insurance date</label> </br>
+    //   <input type="month" /> </br>
+    //   <slot></slot>
+    // `;
   }
 
+
+  setPlayer(name){
+    this.selectedPlayer = name;
+    this.requestUpdate();
+  }
   _onClick() {
     this.count++;
     this.dispatchEvent(new CustomEvent('count-changed'));
