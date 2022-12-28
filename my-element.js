ddfeed12de5e372e058e5b66a6d6055d5f96585d
JMLatino <38223339+JMLatino@users.noreply.github.com>
2022-12-28 14:52:11 +0000

    added stuff

--- my-element.js
+++ my-element.js
@@ -4,7 +4,18 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-import {LitElement, html, css} from 'lit';
+import { LitElement, html, css } from 'lit';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+
+
+const appInsights = new ApplicationInsights({
+  config: {
+    
+  },
+});
+
+appInsights.config.instrumentationKey = '6aaa2b34-7484-4df5-bae0-4036f120703d';
+appInsights.loadAppInsights();
 
 /**
  * An example element.
@@ -31,13 +42,13 @@ export class MyElement extends LitElement {
        * The name to say "Hello" to.
        * @type {string}
        */
-      name: {type: String},
+      name: { type: String },
 
       /**
        * The number of times the button has been clicked.
        * @type {number}
        */
-      count: {type: Number},
+      count: { type: Number },
     };
   }
 
@@ -46,32 +57,47 @@ export class MyElement extends LitElement {
     this.name = 'World';
     this.count = 0;
     this.players = [
-      {name: 'Ronaldo', img: 'https://cdn.nos.nl/image/2022/11/24/919035/768x576a.jpg'},
-      {name: 'Messi', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/640px-Lionel_Messi_WC2022.jpg'},
-      {name: 'Mbappé', img: 'https://ronaldo.com/wp-content/uploads/2019/09/GettyImages-1159265415.jpg'},
-      {name: 'Neymar', img: 'https://media.nu.nl/m/2m2x54gamlhw_wd854/neymar.jpg'},
-      {name: 'de Jong', img: 'https://vp.cdn.pxr.nl/news/2022/12/20/v2_large_97d9760e1867827bd773a20a4af2cff37de5314c.jpg'},
-      {name: 'Hakimi', img: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltf4389e5af01ea4c3/60dab80f892a730f58805a04/ae5d85fe4883d988d81024996c9b8892d664c4e4.jpg'},
-      {name: 'Modrić', img: 'https://img.a.transfermarkt.technology/portrait/header/27992-1661426133.jpg?lm=1'},
-      
+      { name: 'Ronaldo', img: 'https://cdn.nos.nl/image/2022/11/24/919035/768x576a.jpg' },
+      { name: 'Messi', img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Lionel_Messi_WC2022.jpg/640px-Lionel_Messi_WC2022.jpg' },
+      { name: 'Mbappé', img: 'https://ronaldo.com/wp-content/uploads/2019/09/GettyImages-1159265415.jpg' },
+      { name: 'Neymar', img: 'https://media.nu.nl/m/2m2x54gamlhw_wd854/neymar.jpg' },
+      { name: 'de Jong', img: 'https://vp.cdn.pxr.nl/news/2022/12/20/v2_large_97d9760e1867827bd773a20a4af2cff37de5314c.jpg' },
+      { name: 'Hakimi', img: 'https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltf4389e5af01ea4c3/60dab80f892a730f58805a04/ae5d85fe4883d988d81024996c9b8892d664c4e4.jpg' },
+      { name: 'Modrić', img: 'https://img.a.transfermarkt.technology/portrait/header/27992-1661426133.jpg?lm=1' },
+
     ];
     this.selectedPlayer = '';
+    appInsights.startTrackPage('players overview')
+  }
+
+  firstUpdated(){
+    appInsights.stopTrackPage('players overview');
   }
 
   render() {
     return html`
+    ${this.players
+        .map(
+          player => this.selectedPlayer === player.name
+            ? html`
+          <div ><button class="btn-primary" @click="${() => this.setPlayer(player.name)}">
+            <h5 class="card-title">${player.name}</h5>
+            <img width="200" src="${player.img}" class="card-img-top" >
+          </button></div>
+          `
+            : ``)
+      }
       ${this.players
         .map(
-          player => this.selectedPlayer.length == 0 || this.selectedPlayer === player.name 
-          ?  html`
+          player =>  html`
           <button @click="${() => this.setPlayer(player.name)}">
             <h5 class="card-title">${player.name}</h5>
             <img width="200" src="${player.img}" class="card-img-top" >
           </button>
           `
-          :``)
-        }`
-
+          )
+      }
+      `
     // return html`
     //   <h1>Request Insurance</h1>
     //   <label>Name</label> </br>
@@ -85,8 +111,10 @@ export class MyElement extends LitElement {
   }
 
 
-  setPlayer(name){
+  setPlayer(name) {
     this.selectedPlayer = name;
+    appInsights.trackEvent({ name: 'player selected', properties: {selectedPlayer: name}})
+    appInsights.trackEvent({ name: 'player selected', properties: {selectedPlayer: name}})
     this.requestUpdate();
   }
   _onClick() {
